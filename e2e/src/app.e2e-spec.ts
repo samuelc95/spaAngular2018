27301d3f2bdb473ca2759f81f43b7f04c916a9bd
Luis Peixoto <1151111@isep.ipp.pt>
2018-12-09 22:07:31 +0000

    teste e2e titulos

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -7,8 +7,54 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getTitleText()).toEqual('ArqsiIt3');
+  it('should display produto title', () => {
+    page.navigateToProduto();
+    expect(page.getTitleText()).toEqual('Listagem Produtos');
   });
+
+  it('should display restricao title', () => {
+    page.navigateToRestricao();
+    expect(page.getTitleText()).toEqual('Listagem Restrições');
+  });
+
+  it('should display categoria title', () => {
+    page.navigateToCategoria();
+    expect(page.getTitleText()).toEqual('Listagem Categorias');
+  });
+
+  it('should display material title', () => {
+    page.navigateToMaterial();
+    expect(page.getTitleText()).toEqual('Listagem Materiais');
+  });
+
+  it('should display acabamento title', () => {
+    page.navigateToAcabamento();
+    expect(page.getTitleText()).toEqual('Listagem Acabamentos');
+  });
+
+  it('should display agregacao title', () => {
+    page.navigateToAgregacao();
+    expect(page.getTitleText()).toEqual('Listagem Agregações');
+  });
+
+  it('should display Mat/Acabamento title', () => {
+    page.navigateToMatAcabamento();
+    expect(page.getTitleText()).toEqual('Listagem Materiais/Acabamentos');
+  });
+
+  it('should display dimensao title', () => {
+    page.navigateToDimensao();
+    expect(page.getTitleText()).toEqual('Listagem Dimensões');
+  });
+
+  it('should display ItemProduto title', () => {
+    page.navigateToItemProduto();
+    expect(page.getTitleText()).toEqual('Listagem Itens Produto');
+  });
+
+  it('should display encomenda title', () => {
+    page.navigateToEncomenda();
+    expect(page.getTitleText()).toEqual('Listagem Encomendas');
+  });
+
 });
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -2,10 +2,51 @@ import { browser, by, element } from 'protractor';
 
 export class AppPage {
   navigateTo() {
+    return browser.get('/');
+  }
+
+  navigateToProduto() {
     return browser.get('/produto');
   }
 
-  getTitleText() {
-    return element(by.class('title')).getText();
+  navigateToRestricao() {
+    return browser.get('/restricao');
   }
+
+  navigateToCategoria() {
+    return browser.get('/categoria');
+  }
+
+  navigateToMaterial() {
+    return browser.get('/material');
+  }
+
+  navigateToAcabamento() {
+    return browser.get('/acabamento');
+  }
+
+  navigateToMatAcabamento() {
+    return browser.get('/materialAcabamento');
+  }
+
+  navigateToAgregacao() {
+    return browser.get('/agregacao');
+  }
+
+  navigateToDimensao() {
+    return browser.get('/dimensao');
+  }
+
+  navigateToItemProduto() {
+    return browser.get('/itemProduto');
+  }
+
+  navigateToEncomenda() {
+    return browser.get('/encomenda');
+  }
+
+  getTitleText() {
+    return element(by.className('title')).getText();
+  }
+
 }
